3afe14695d0288d5b5b888d41d1633775de0de7a
Angelina G <glinaleloo@gmail.com>
2024-06-08 22:38:10 +0400

    3rd commit

--- src/Massiv2.java
+++ src/Massiv2.java
@@ -9,6 +9,7 @@ public class Massiv2 {
             //На каждой следующей итерации i увеличивается на 1, таким образом мы проходимся по всему массиву
             System.out.println(nums[i]);
             // jngdfbdlkf
+            //kvjdfnvkdfjv
         }
     }
 }
